01df6e93992b973440060c8544f842726040068a
popipopka <eg.belykh@yandex.ru>
2023-11-29 03:36:09 +0300

    Linear to cosine interpolate

--- src/main/java/com/meow/rasterization/Rasterization.java
+++ src/main/java/com/meow/rasterization/Rasterization.java
@@ -14,16 +14,15 @@ public class Rasterization {
                                   final int x0, final int y0,
                                   final double angle1, final double angle2,
                                   final double r,
-                                  Color start, Color end) {
+                                  Color c1, Color c2) {
         PixelWriter writer = graphicsContext.getPixelWriter();
 
-        final double minAngle = Math.min(angle1, angle2);
-        final double maxAngle = Math.max(angle1, angle2);
+        double minAngle = simplifyAngle(Math.min(angle1, angle2));
+        double maxAngle = simplifyAngle(Math.max(angle1, angle2));
 
         int xLeft;
         int xRight;
         double sqrt;
-        boolean isBelongsToSector;
         for (int y = (int) (y0 - r + 1); y < (int) (y0 + r); y++) {
             sqrt = Math.round(Math.sqrt(Math.pow(r, 2) - Math.pow((y - y0), 2)));
 
@@ -31,23 +30,26 @@ public class Rasterization {
             xRight = (int) (x0 + sqrt);
 
             for (int x = xLeft; x <= xRight; x++) {
-                isBelongsToSector = belongsToSector(minAngle, maxAngle, x0, y0, x, y, r);
-
-                if (isBelongsToSector) {
-                    writer.setColor(x, y, interpolateColor(start, end, r, x0, y0, x, y));
+                if (belongsToSector(minAngle, maxAngle, x0, y0, x, y, r)) {
+                    writer.setColor(x, y, interpolateColor(c1, c2, r, x0, y0, x, y));
                 }
             }
         }
     }
 
-    // Зеркально отражает относительно Oy и делает его значение от 0 до 360
-    public static double normalizeAngle(double angle) {
-        return 360 - (angle + 360) % 360;
+    private static double mirrorAngleByOy(double angle) {
+        return 360 - simplifyAngle(angle);
+    }
+
+    private static double simplifyAngle(double angle) {
+        if (angle != 0 && angle % 360 == 0) return 360;
+
+        return (angle + 360) % 360;
     }
 
     private static boolean belongsToSector(double minAngle, double maxAngle, int x0, int y0, int x, int y, double radius) {
         double r = distanceBetweenPoints(x0, y0, x, y);
-        double angle = normalizeAngle(Math.toDegrees(Math.atan2((y - y0), (x - x0))));
+        double angle = mirrorAngleByOy(Math.toDegrees(Math.atan2((y - y0), (x - x0))));
 
         return minAngle <= angle && angle <= maxAngle && r <= radius;
     }
@@ -56,7 +58,7 @@ public class Rasterization {
         return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
 
-    public static Color interpolateColor(Color c1, Color c2, double radius, int x0, int y0, int x, int y) {
+    private static Color interpolateColor(Color c1, Color c2, double radius, int x0, int y0, int x, int y) {
         double r1 = c1.getRed();
         double g1 = c1.getGreen();
         double b1 = c1.getBlue();
@@ -67,10 +69,15 @@ public class Rasterization {
 
         double ratio = distanceBetweenPoints(x0, y0, x, y) / radius;
 
-        double r = r1 * (1 - ratio) + r2 * ratio;
-        double g = g1 * (1 - ratio) + g2 * ratio;
-        double b = b1 * (1 - ratio) + b2 * ratio;
+        double r = cosineInterpolate(r1, r2, ratio);
+        double g = cosineInterpolate(g1, g2, ratio);
+        double b = cosineInterpolate(b1, b2, ratio);
 
         return new Color(r, g, b, 1.0);
+    }
+
+    private static double cosineInterpolate(double a, double b, double ratio) {
+        double f = (1 - Math.cos(ratio * Math.PI)) * 0.5;
+        return a * (1 - f) + b * ratio;
     }
 }
